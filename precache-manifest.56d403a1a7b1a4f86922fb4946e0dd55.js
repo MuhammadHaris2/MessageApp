a9c4e7bcf176441f6bdcef08bb05814c918c736c
MuhammadHaris2 <mhari8339@gmail.com>
2020-09-01 10:57:35 +0500

    Updates

---
+++ precache-manifest.56d403a1a7b1a4f86922fb4946e0dd55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e88c6c97c01eae5ff8acc25fd210bf5",
+    "url": "/MessageApp/index.html"
+  },
+  {
+    "revision": "4495d459bad13c42cf5d",
+    "url": "/MessageApp/static/css/main.c8191027.chunk.css"
+  },
+  {
+    "revision": "f35858503d5cd0e04213",
+    "url": "/MessageApp/static/js/2.df9ff396.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/MessageApp/static/js/2.df9ff396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4495d459bad13c42cf5d",
+    "url": "/MessageApp/static/js/main.96a7a3bf.chunk.js"
+  },
+  {
+    "revision": "9e8c56e1550067161294",
+    "url": "/MessageApp/static/js/runtime-main.9d1c4fa9.js"
+  }
+]);
