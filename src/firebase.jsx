73031fb5cdbeaa6020828f59d0017f5e1d4b4487
MuhammadHaris2 <47888959+MuhammadHaris2@users.noreply.github.com>
2020-10-10 10:32:26 +0500

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 const messengerAPP = firebase.initializeApp({
-  apiKey: "AIzaSyCaXAIZgKQx7jvF9gd2D5hrEJo_WIKTRXM",
-  authDomain: "messenger-apps-8c597.firebaseapp.com",
-  databaseURL: "https://messenger-apps-8c597.firebaseio.com",
-  projectId: "messenger-apps-8c597",
-  storageBucket: "messenger-apps-8c597.appspot.com",
-  messagingSenderId: "968170030969",
-  appId: "1:968170030969:web:011add32d89b187960d1a8",
-  measurementId: "G-C8QW9JPN1R"
+  apiKey: "AIzaSyAFI_RftMmNuKBEtDs0pZ6oki16xEaWgBE",
+  authDomain: "messenger-clone-3085d.firebaseapp.com",
+  databaseURL: "https://messenger-clone-3085d.firebaseio.com",
+  projectId: "messenger-clone-3085d",
+  storageBucket: "messenger-clone-3085d.appspot.com",
+  messagingSenderId: "922407850543",
+  appId: "1:922407850543:web:d27e5b8ba8b4e302882cda",
+  measurementId: "G-PX9PPZNR22"
 })
 
 const db = messengerAPP.firestore();
